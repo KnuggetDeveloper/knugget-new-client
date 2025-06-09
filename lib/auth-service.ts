ecc5b96380af0fa1e130d02f264abf867f07e328
mrinal-mann <mrinal.mann24@gmail.com>
2025-06-09 09:49:50 +0530

    Update base URL environment variable in AuthService to use NEXT_PUBLIC_API_BASE_URL

--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -14,7 +14,7 @@ class AuthService {
   private baseUrl: string
 
   constructor() {
-    this.baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+    this.baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000'
   }
 
   private async makeRequest<T>(
